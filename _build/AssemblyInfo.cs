7fdd9505dfbeff2a0bd6d11811a3492a6a4d6fba
Svjatoslav Prokipets <Svjatoslav Prokipets@localhost>
2015-05-10 13:45:38 +0300

    branch creation 0.1.1.x

--- _build/AssemblyInfo.cs
+++ _build/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.9.0.0")]
-[assembly: AssemblyFileVersion("0.9.0.0")]
+[assembly: AssemblyVersion("0.1.1.0")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
 
 
