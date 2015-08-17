5c484911376e05af528204c80fe3dcf32211b9a7
Svjatoslav Prokipets <Svjatoslav Prokipets@localhost>
2015-08-17 22:36:32 +0300

    [SK-1] Initial project creation

--- EventStoreKit/SearchOptions/SearchOptions.cs
+++ EventStoreKit/SearchOptions/SearchOptions.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using EventStoreKit.Constants;
 using EventStoreKit.Utility;
 using Newtonsoft.Json;
