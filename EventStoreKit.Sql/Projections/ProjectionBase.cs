c2351563bad52be0b28453aa8b0dba60a60f7757
svjatpro <svjatpro@localhost>
2016-02-08 17:24:05 +0200

    [SK-8] Remove bltoolkit dependency ( example in documentation ) - separate dll for Sql support

--- EventStoreKit.Sql/Projections/ProjectionBase.cs
+++ EventStoreKit.Sql/Projections/ProjectionBase.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Reactive.Concurrency;
 using System.Runtime.Remoting.Messaging;
-using System.Threading.Tasks;
 using EventStoreKit.Messages;
 using EventStoreKit.Projections;
 using EventStoreKit.Sql.ProjectionTemplates;
