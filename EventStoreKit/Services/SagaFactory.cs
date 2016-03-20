ac22f49c09fe287fa57885258cf6c9f36e6fd4cf
Svjatoslav Prokipets <svjatpro@users.noreply.github.com>
2016-03-21 00:45:41 +0200

    [SK-9] Extend IConstructSagas

--- EventStoreKit/Services/SagaFactory.cs
+++ EventStoreKit/Services/SagaFactory.cs
@@ -1,21 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
 using CommonDomain;
 using CommonDomain.Persistence;
 
 namespace EventStoreKit.Services
 {
-    public class SagaFactory : IConstructSagas
+    public interface ISagaFactory
     {
+        SagaFactory RegisterSagaConstructor<TSaga>( Func<string,TSaga> sagaBuilder ) where TSaga : ISaga;
+    }
+
+    public class SagaFactory : IConstructSagas, ISagaFactory
+    {
+        private readonly Dictionary<Type, Func<string, ISaga>> SagaBuilders = new Dictionary<Type, Func<string, ISaga>>();
+
         #region Implementation of IConstructSagas
 
         public ISaga Build( Type type, string id )
         {
-            var entity = Activator.CreateInstance( type, id ) as ISaga;
+            var entity = 
+                SagaBuilders.ContainsKey( type ) ?
+                SagaBuilders[type]( id ) :
+                Activator.CreateInstance( type, id ) as ISaga;
             return entity;
         }
 
         #endregion
 
-        //todo: public void RegisterSagaInitializer( Type sagaType, () => ... )
+        public SagaFactory RegisterSagaConstructor<TSaga>( Func<string,TSaga> sagaBuilder ) where TSaga : ISaga
+        {
+            SagaBuilders[typeof (TSaga)] = sagaId => sagaBuilder( sagaId );
+            return this;
+        }
     }
 }
