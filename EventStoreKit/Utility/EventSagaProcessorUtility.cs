93f23541d4c4a027d6424b8de383ce9255560fc4
svjatpro <svjatpro@localhost>
2016-05-12 21:03:14 +0300

    [SK-13] Refactor SagaProcess utility methods

--- EventStoreKit/Utility/EventSagaProcessorUtility.cs
+++ EventStoreKit/Utility/EventSagaProcessorUtility.cs
@@ -12,69 +12,11 @@ namespace EventStoreKit.Utility
     /// </summary>
     public static class EventSagaProcessorUtility
     {
-        /// <summary>
-        /// Resolve ( if null ) New Saga instance, process Message with Saga and save Saga
-        /// </summary>
-        /// <typeparam name="TSaga">Saga Type</typeparam>
-        /// <param name="message">Message to process</param>
-        /// <param name="repository">SagaRepository</param>
-        /// <param name="saga">Saga instance ( if exist )</param>
-        /// <returns>Saga instance</returns>
-        public static TSaga ProcessAndSaveSaga<TSaga>( this Message message, ISagaRepository repository, TSaga saga = null )
-            where TSaga : class, ISaga, new()
-        {
-            saga = saga ?? Activator.CreateInstance<TSaga>();
-            message.ProcessAndSaveSaga( saga, repository );
-            return saga;
-        }
+        #region Private methods
 
-        /// <summary>
-        /// Resolve ( if null ) Saga instance by Id, process Message with Saga and save Saga
-        /// </summary>
-        /// <typeparam name="TSaga">Saga Type</typeparam>
-        /// <param name="message">Message to process</param>
-        /// <param name="sagaId">Unique Saga identity</param>
-        /// <param name="repository">SagaRepository</param>
-        /// <param name="saga">Saga instance ( if exist )</param>
-        /// <returns>Saga instance</returns>
-        public static TSaga ProcessAndSaveSaga<TSaga>( this Message message, string sagaId, ISagaRepository repository, TSaga saga = null )
-            where TSaga : class, ISaga, new()
-        {
-            saga = saga ?? repository.GetById<TSaga>( sagaId );
-            message.ProcessAndSaveSaga( saga, repository );
-            return saga;
-        }
-
-        /// <summary>
-        /// Resolve ( if null ) Saga instance by custom resolve Func, process Message with Saga and save Saga
-        /// </summary>
-        /// <typeparam name="TSaga">Saga Type</typeparam>
-        /// <param name="message">Message to process</param>
-        /// <param name="resolveSaga">Custom Func to resolve / create Saga</param>
-        /// <param name="repository">SagaRepository</param>
-        /// <param name="saga">Saga instance ( if exist )</param>
-        /// <returns>Saga instance</returns>
-        public static TSaga ProcessAndSaveSaga<TSaga>( this Message message, Func<TSaga> resolveSaga, ISagaRepository repository, TSaga saga = null )
+        private static void PrepareMessages<TSaga>( this TSaga saga, Message message )
             where TSaga : class, ISaga
         {
-            saga = saga ?? resolveSaga();
-            message.ProcessAndSaveSaga( saga, repository );
-            return saga;
-        }
-
-        /// <summary>
-        /// Process Message with Saga instance, and then save saga
-        /// </summary>
-        /// <typeparam name="TSaga">Saga Type</typeparam>
-        /// <param name="message">Messag to process</param>
-        /// <param name="saga">Instance of Saga</param>
-        /// <param name="repository">SagaRepository to save Saga after process message</param>
-        /// <returns>Saga instance</returns>
-        public static TSaga ProcessAndSaveSaga<TSaga>( this Message message, TSaga saga, ISagaRepository repository )
-            where TSaga : class, ISaga
-        {
-            saga.Transition( message );
-
             foreach ( var msg in saga.GetUncommittedEvents() )
             {
                 msg.OfType<DomainEvent>().Do( @event =>
@@ -91,7 +33,47 @@ namespace EventStoreKit.Utility
                     cmd.CreatedBy = message.CreatedBy;
                 } );
             }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Resolve ( if null ) Saga instance by Id, process Message with Saga and save Saga
+        /// </summary>
+        /// <typeparam name="TSaga">Saga Type</typeparam>
+        /// <param name="message">Message to process</param>
+        /// <param name="sagaId">Unique Saga identity</param>
+        /// <param name="repository">SagaRepository</param>
+        /// <param name="saga">Saga instance ( if exist )</param>
+        /// <returns>Saga instance</returns>
+        public static TSaga ProcessAndSaveSaga<TSaga>( this Message message, string sagaId, ISagaRepository repository, TSaga saga = null )
+            where TSaga : class, ISaga, new()
+        {
+            saga = saga ?? repository.GetById<TSaga>( sagaId );
+            saga.Transition( message );
+            saga.PrepareMessages( message );
             repository.Save( saga, Guid.NewGuid(), a => { } );
+
+            return saga;
+        }
+
+        /// <summary>
+        /// Resolve ( if null ) Saga instance by custom resolve Func, process Message with Saga and save Saga
+        /// </summary>
+        /// <typeparam name="TSaga">Saga Type</typeparam>
+        /// <param name="message">Message to process</param>
+        /// <param name="resolveSaga">Custom Func to resolve / create Saga</param>
+        /// <param name="repository">SagaRepository</param>
+        /// <param name="saga">Saga instance ( if exist )</param>
+        /// <returns>Saga instance</returns>
+        public static TSaga ProcessAndSaveSaga<TSaga>( this Message message, Func<TSaga> resolveSaga, ISagaRepository repository, TSaga saga = null )
+            where TSaga : class, ISaga
+        {
+            saga = saga ?? resolveSaga();
+            saga.Transition( message );
+            saga.PrepareMessages( message );
+            repository.Save( saga, Guid.NewGuid(), a => { } );
+
             return saga;
         }
 
@@ -110,7 +92,8 @@ namespace EventStoreKit.Utility
         {
             saga = saga ?? repository.GetById<TSaga>( sagaId );
             saga.Transition( message );
-            
+            saga.PrepareMessages( message );
+
             Enumerable.OfType<DomainCommand>( saga.GetUndispatchedMessages() )
                 .ToList()
                 .ForEach( commandBus.Send );
@@ -120,9 +103,32 @@ namespace EventStoreKit.Utility
 
             return saga;
         }
-        public static TSaga ProcessSaga<TSaga>( this Message message, ISagaRepository repository, ICommandBus commandBus ) where TSaga : class, ISaga, new()
+
+        /// <summary>
+        /// Resolve and process Saga message, then dispatch commands and save saga, headers doesn't contain commands!
+        /// </summary>
+        /// <typeparam name="TSaga">Saga Type</typeparam>
+        /// <param name="message">Message to process</param>
+        /// <param name="sagaId">Unique Saga Identity</param>
+        /// <param name="repository">SagaRepository to resolve empty saga</param>
+        /// <param name="commandBus">command bus</param>
+        /// <param name="saga">Instance of saga ( If already resolved )</param>
+        /// <returns>Saga instance</returns>
+        public static TSaga ProcessSagaCustom<TSaga>( this Message message, string sagaId, ISagaRepository repository, ICommandBus commandBus, TSaga saga = null )
+            where TSaga : class, ISaga
         {
-            return message.ProcessSaga( string.Empty, repository, commandBus, new TSaga() );
+            saga = saga ?? repository.GetById<TSaga>( sagaId );
+            saga.Transition( message );
+            saga.PrepareMessages( message );
+
+            Enumerable.OfType<DomainCommand>( saga.GetUndispatchedMessages() )
+                .ToList()
+                .ForEach( commandBus.Send );
+
+            saga.ClearUndispatchedMessages();
+            repository.Save( saga, Guid.NewGuid(), a => { } );
+
+            return saga;
         }
     }
 }
