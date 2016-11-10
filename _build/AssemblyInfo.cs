b626182e3baebcffadf27f1eb0d9879501632013
Svjatoslav Prokipets <svjatpro@users.noreply.github.com>
2016-11-10 10:32:27 +0200

    [SK-28] Move to last nEventStore and synchronize versioning with nEventStore versions ( ?rename )

--- _build/AssemblyInfo.cs
+++ _build/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.2.5.0")]
-[assembly: AssemblyFileVersion("0.2.5.0")]
+[assembly: AssemblyVersion("5.2.0.0")]
+[assembly: AssemblyFileVersion("5.2.0.0")]
