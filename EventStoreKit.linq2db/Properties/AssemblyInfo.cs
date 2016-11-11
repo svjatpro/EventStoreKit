6adc6e0c389b7db9fb4735c30f7ce5835849d86f
svjatpro <svjatpro@localhost>
2016-11-12 00:26:35 +0200

    [SK-29] Make secondary projects & packages: log4net, linq2db

--- EventStoreKit.linq2db/Properties/AssemblyInfo.cs
+++ EventStoreKit.linq2db/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EventStoreKit.Autofac")]
+[assembly: AssemblyTitle("EventStoreKit.linq2db")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EventStoreKit.Autofac")]
+[assembly: AssemblyProduct("EventStoreKit.linq2db")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("46ec2be5-6f0b-4d8d-a3fc-acd1a89ddc34")]
+[assembly: Guid("94310ea7-f266-4437-9b12-6ab970fe70ea")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "1.0.1.0" )]
-[assembly: AssemblyFileVersion( "1.0.1.0" )]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
