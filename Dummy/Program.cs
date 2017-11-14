83e976f53a86442f65a0197a40593560b67712e6
Svjatoslav Prokipets <svjatpro@gmail.com>
2017-11-15 00:33:00 +0200

    Move all configuration outside of module

--- Dummy/Program.cs
+++ Dummy/Program.cs
@@ -1,15 +1,96 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reactive.Concurrency;
+using CommonDomain.Core;
+using EventStoreKit.Aggregates;
+using EventStoreKit.DbProviders;
+using EventStoreKit.Handler;
+using EventStoreKit.Logging;
+using EventStoreKit.Messages;
+using EventStoreKit.Projections;
+using EventStoreKit.Services;
+using EventStoreKit.Services.Configuration;
 
 namespace Dummy
 {
+    public class GreetCommand : DomainCommand
+    {
+        public string Object { get; set; }
+    }
+
+    public class GreetedEvent : DomainEvent
+    {
+        public string HelloMessage { get; set; }
+    }
+
+    public class Speaker : TrackableAggregateBase
+    {
+        private void Apply( GreetedEvent message ){}
+
+        public Speaker( Guid id )
+        {
+            Id = id;
+            Register<GreetedEvent>( Apply );
+        }
+
+        public void Greet( string objectName )
+        {
+            RaiseEvent( new GreetedEvent
+            {
+                Id = Id,
+                HelloMessage = $"Hello, {objectName}!"
+            } );
+        }
+    }
+
+    public class SpeakerHandler : ICommandHandler<GreetCommand, Speaker>
+    {
+        public void Handle( GreetCommand cmd, CommandHandlerContext<Speaker> context )
+        {
+            context.Entity.Greet( cmd.Object );
+        }
+    }
+
+    public class GreetingModel
+    {
+        public Guid SpeakerId { get; set; }
+        public string Message { get; set; }
+    }
+
+    public class GreetingsProjection : SqlProjectionBase<GreetingModel>,
+        IEventHandler<GreetedEvent>
+    {
+        public void Handle( GreetedEvent message )
+        {
+            DbProviderFactory.Run( db => db.Insert( 
+                new GreetingModel
+                {
+                    SpeakerId = message.Id,
+                    Message = message.HelloMessage
+                } ) );
+        }
+
+        public GreetingsProjection( ILogger logger, IScheduler scheduler, IEventStoreConfiguration config, IDbProviderFactory dbProviderFactory ) : 
+            base( logger, scheduler, config, dbProviderFactory )
+        {
+        }
+
+        public List<GreetingModel> GetAllMessages()
+        {
+            return DbProviderFactory.Run( db => db.Query<GreetingModel>().ToList() );
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            var service = new EventStoreKitService();
+            service.RegisterCommandHandler<SpeakerHandler>();
+            service.Initialize();
+
+
         }
     }
 }
---
+++ EventStoreKit/Services/EventStoreKitService.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using CommonDomain.Persistence;
+using EventStoreKit.Aggregates;
+using EventStoreKit.CommandBus;
+using EventStoreKit.Handler;
+using EventStoreKit.Messages;
+
+namespace EventStoreKit.Services
+{
+    public class EventStoreKitService
+    {
+        private readonly IEventDispatcher Dispatcher;
+
+        private void RegisterCommandHandler<TCommand, TEntity>()
+            // ReSharper restore UnusedMember.Local
+            where TCommand : DomainCommand
+            where TEntity : class, ITrackableAggregate
+        {
+            // register Action as handler to dispatcher
+            var repositoryFactory = Container.Resolve<Owned<Func<IRepository>>>();
+            var handlerFactory = Container.Resolve<Owned<Func<ICommandHandler<TCommand, TEntity>>>>();
+
+            var handleAction = new Action<TCommand>( cmd =>
+            {
+                var repository = repositoryFactory.Value();
+                var handler = handlerFactory.Value();
+
+                if ( cmd.Created == default( DateTime ) )
+                    cmd.Created = DateTime.Now;
+                if ( cmd.CreatedBy == Guid.Empty && CurrentUserProvider.CurrentUserId != null )
+                    cmd.CreatedBy = CurrentUserProvider.CurrentUserId.Value;
+                var context = new CommandHandlerContext<TEntity>
+                {
+                    Entity = repository.GetById<TEntity>( cmd.Id )
+                };
+                if ( cmd.CreatedBy != Guid.Empty )
+                    context.Entity.IssuedBy = cmd.CreatedBy;
+                else
+                    CurrentUserProvider.CurrentUserId.Do( userId => context.Entity.IssuedBy = userId.GetValueOrDefault() );
+
+                handler.Handle( cmd, context );
+                Logger.Info( "{0} processed; version = {1}", cmd.GetType().Name, cmd.Version );
+                repository.Save( context.Entity, IdGenerator.NewGuid() );
+            } );
+            Dispatcher.RegisterHandler( handleAction );
+        }
+
+        public void RegisterCommandHandler<SpeakerHandler>()
+        {
+            
+        }
+
+        private EventStoreKitService()
+        {
+            
+        }
+
+        public static EventStoreKitService Initialize()
+        {
+            var service = new EventStoreKitService();
+
+            builder.RegisterType<MessageDispatcher>()
+                .As<IEventPublisher>()
+                .As<IEventDispatcher>()
+                .As<ICommandBus>()
+                .SingleInstance();
+
+            // handlers for Aggregates
+
+            return service;
+        }
+    }
+}
