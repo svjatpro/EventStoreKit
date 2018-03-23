572b1120200e623d17f3299e0af1d1b13441241e
Svjatoslav Prokipets <svip@isddesign.com>
2018-03-23 20:34:20 +0200

    move EventStore intergation to external module

--- EventStoreKit.Core/EventStore/IEventStore.cs
+++ EventStoreKit.Core/EventStore/IEventStore.cs
@@ -3,21 +3,115 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EventStoreKit.DbProviders;
+using EventStoreKit.Logging;
+using EventStoreKit.Messages;
+using EventStoreKit.Projections;
+using EventStoreKit.Services;
+using EventStoreKit.Utility;
+using NEventStore;
+using NEventStore.Dispatcher;
+using NEventStore.Persistence.Sql;
+using NEventStore.Persistence.Sql.SqlDialects;
 
 namespace EventStoreKit.Core.EventStore
 {
+
     public interface IEventStore
     {
+        event EventHandler<MessageEventArgs> MessagePublished;
 
+        void AppendToStream( Message message );
     }
 
     public class NEventStoreAdapter : IEventStore
     {
+        private readonly IStoreEvents StoreEvents;
         
+        public NEventStoreAdapter( Wireup wireup = null, IDataBaseConfiguration configuration = null, ILoggerFactory loggerFactory = null )
+        {
+            if( wireup == null )
+            {
+                wireup = Wireup.Init();
+                if (configuration == null)
+                    configuration = new DataBaseConfiguration(DataBaseConnectionType.None, null);
+            }
+
+            if ( configuration != null )
+            {
+                if ( configuration.DataBaseConnectionType == DataBaseConnectionType.None )
+                {
+                    wireup = wireup.UsingInMemoryPersistence();
+                }
+                else
+                {
+                    var persistanceWireup =
+                        configuration.ConfigurationString != null ? 
+                        wireup.UsingSqlPersistence( configuration.ConfigurationString ) : 
+                        wireup.UsingSqlPersistence( null, configuration.ConnectionProviderName, configuration.ConnectionString );
+
+                    var dialectTypeMap = new Dictionary<DataBaseConnectionType, Type>
+                    {
+                        {DataBaseConnectionType.MsSql2000, typeof(MsSqlDialect)},
+                        {DataBaseConnectionType.MsSql2005, typeof(MsSqlDialect)},
+                        {DataBaseConnectionType.MsSql2008, typeof(MsSqlDialect)},
+                        {DataBaseConnectionType.MsSql2012, typeof(MsSqlDialect)},
+                        {DataBaseConnectionType.MySql, typeof(MySqlDialect)},
+                        {DataBaseConnectionType.SqlLite, typeof(SqliteDialect)}
+                    };
+                    wireup = persistanceWireup
+                        .WithDialect( (ISqlDialect) Activator.CreateInstance( dialectTypeMap[configuration.DataBaseConnectionType] ) )
+                        .PageEvery( 1024 )
+                        .InitializeStorageEngine()
+                        .UsingJsonSerialization();
+                }
+            }
+
+            if ( loggerFactory != null )
+            {
+                wireup = wireup.LogTo( type => loggerFactory.Create<NEventStoreAdapter>() );
+            }
+
+            StoreEvents = new AsynchronousDispatchSchedulerWireup(
+                    wireup,
+                    new DelegateMessageDispatcher( DispatchCommit ),
+                    DispatcherSchedulerStartup.Auto )
+                .UsingEventUpconversion()
+                //.WithConvertersFrom( AppDomain.CurrentDomain.GetAssemblies() /*.Where( a => a.FullName.StartsWith( "Code.CL.Domain" ) )*/.ToArray() )
+                .Build();
+        }
+
+        private void DispatchCommit( ICommit commit )
+        {
+            foreach ( var message in commit.Events )
+            {
+                message.ProcessEvent( e =>
+                {
+                    e.Version = commit.StreamRevision;
+                    e.BucketId = commit.BucketId;
+                    e.CheckpointToken = commit.CheckpointToken;
+
+                    MessagePublished.Execute( this, new MessageEventArgs( e ) );
+                } );
+            }
+        }
+
+        public event EventHandler<MessageEventArgs> MessagePublished;
+
+        public void AppendToStream(Message message)
+        {
+            using (var stream = StoreEvents.CreateStream(Bucket.Default, message.Id))
+            {
+                
+            }
+        }
     }
 
     public class DomainHub // todo: rename it
     {
-        
+        public DomainHub()
+        {
+            
+        }
     }
 }
---
+++ EventStoreKit.NEventStore.Tests/NEventStoreAdapterTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EventStoreKit.Core.EventStore;
+using EventStoreKit.DbProviders;
+using EventStoreKit.Services;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace EventStoreKit.NEventStore.Tests
+{
+    [TestFixture]
+    public class NEventStoreAdapterTests
+    {
+        [Test]
+        public void Test1()
+        {
+            var adapter = new NEventStoreAdapter();
+
+            adapter.MessagePublished += (o, msg) =>
+            {
+                Console.WriteLine( msg.Message.Id );
+            };
+
+
+        }
+    }
+}
