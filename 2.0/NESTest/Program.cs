58a8a376aae8d315f95aaa21826ef86476a24d9d
Svjatoslav Prokipets <svjatpro@gmail.com>
2019-08-12 00:04:10 +0300

    reload 2.0 prototype

---
+++ 2.0/NESTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data.SqlClient;
+using NEventStore;
+using NEventStore.Conversion;
+using NEventStore.Persistence.Sql;
+using NEventStore.Persistence.Sql.SqlDialects;
+using NEventStore.Serialization.Json;
+
+namespace NESTest
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            var store = Wireup.Init()
+                .UsingSqlPersistence( new NetStandardConnectionFactory(SqlClientFactory.Instance, "osbb" ))
+                .WithDialect( new MySqlDialect() )
+                .InitializeStorageEngine()
+                .UsingJsonSerialization()
+                .Compress()
+                //.EncryptWith(  )
+                //.HookIntoPipelineUsing()
+                .Build();
+
+
+        }
+    }
+}
