c021fbb0bb38fb6820ac69ea9f503cfdc4e26fce
Svjatoslav Prokipets <svjatpro@gmail.com>
2019-12-04 23:33:30 +0200

    IEventStore prototype

--- 2.0/EventStoreKit.NEventStore/NEventStore.cs
+++ 2.0/EventStoreKit.NEventStore/NEventStore.cs
@@ -8,88 +8,94 @@ namespace EventStoreKit.NEventStore
 {
     public class NEventStoreAdapter : IEventStore
     {
-        private readonly IIdGenerator IdGenerator;
+        //private readonly IIdGenerator IdGenerator;
         private readonly IStoreEvents StoreEvents;
 
-        public NEventStoreAdapter( Wireup wireup = null, IDataBaseConfiguration configuration = null, ILoggerFactory loggerFactory = null )
+        //public NEventStoreAdapter( Wireup wireup = null, IDataBaseConfiguration configuration = null, ILoggerFactory loggerFactory = null )
+        //{
+        //    IdGenerator = new SequentialIdgenerator();
+
+        //    if ( wireup == null )
+        //    {
+        //        wireup = Wireup.Init();
+        //        if ( configuration == null )
+        //            configuration = new DataBaseConfiguration( DataBaseConnectionType.None, null );
+        //    }
+
+        //    if ( configuration != null )
+        //    {
+        //        if ( configuration.DataBaseConnectionType == DataBaseConnectionType.None )
+        //        {
+        //            wireup = wireup.UsingInMemoryPersistence();
+        //        }
+        //        else
+        //        {
+        //            var persistanceWireup =
+        //                configuration.ConfigurationString != null ?
+        //                wireup.UsingSqlPersistence( configuration.ConfigurationString ) :
+        //                wireup.UsingSqlPersistence( null, configuration.ConnectionProviderName, configuration.ConnectionString );
+
+        //            var dialectTypeMap = new Dictionary<DataBaseConnectionType, Type>
+        //            {
+        //                {DataBaseConnectionType.MsSql2000, typeof(MsSqlDialect)},
+        //                {DataBaseConnectionType.MsSql2005, typeof(MsSqlDialect)},
+        //                {DataBaseConnectionType.MsSql2008, typeof(MsSqlDialect)},
+        //                {DataBaseConnectionType.MsSql2012, typeof(MsSqlDialect)},
+        //                {DataBaseConnectionType.MySql, typeof(MySqlDialect)},
+        //                {DataBaseConnectionType.SqlLite, typeof(SqliteDialect)}
+        //            };
+        //            wireup = persistanceWireup
+        //                .WithDialect( (ISqlDialect) Activator.CreateInstance( dialectTypeMap[configuration.DataBaseConnectionType] ) )
+        //                .PageEvery( 1024 )
+        //                .InitializeStorageEngine()
+        //                .UsingJsonSerialization();
+        //        }
+        //    }
+
+        //    if ( loggerFactory != null )
+        //    {
+        //        wireup = wireup.LogTo( type => loggerFactory.Create<NEventStoreAdapter>() );
+        //    }
+
+        //    StoreEvents = new AsynchronousDispatchSchedulerWireup(
+        //            wireup,
+        //            new DelegateMessageDispatcher( DispatchCommit ),
+        //            DispatcherSchedulerStartup.Auto )
+        //        .UsingEventUpconversion()
+        //        //.WithConvertersFrom( AppDomain.CurrentDomain.GetAssemblies() /*.Where( a => a.FullName.StartsWith( "Code.CL.Domain" ) )*/.ToArray() )
+        //        .Build();
+        //}
+
+        //private void DispatchCommit( ICommit commit )
+        //{
+        //    foreach ( var message in commit.Events )
+        //    {
+        //        message.ProcessEvent( e =>
+        //        {
+        //            e.Version = commit.StreamRevision;
+        //            e.BucketId = commit.BucketId;
+        //            e.CheckpointToken = commit.CheckpointToken;
+
+        //            //MessagePublished.Execute( this, new MessageEventArgs( e ) );
+        //        } );
+        //    }
+        //}
+
+        //public event EventHandler<MessageEventArgs> MessagePublished;
+
+        public NEventStoreAdapter()
         {
-            IdGenerator = new SequentialIdgenerator();
 
-            if ( wireup == null )
-            {
-                wireup = Wireup.Init();
-                if ( configuration == null )
-                    configuration = new DataBaseConfiguration( DataBaseConnectionType.None, null );
-            }
-
-            if ( configuration != null )
-            {
-                if ( configuration.DataBaseConnectionType == DataBaseConnectionType.None )
-                {
-                    wireup = wireup.UsingInMemoryPersistence();
-                }
-                else
-                {
-                    var persistanceWireup =
-                        configuration.ConfigurationString != null ?
-                        wireup.UsingSqlPersistence( configuration.ConfigurationString ) :
-                        wireup.UsingSqlPersistence( null, configuration.ConnectionProviderName, configuration.ConnectionString );
-
-                    var dialectTypeMap = new Dictionary<DataBaseConnectionType, Type>
-                    {
-                        {DataBaseConnectionType.MsSql2000, typeof(MsSqlDialect)},
-                        {DataBaseConnectionType.MsSql2005, typeof(MsSqlDialect)},
-                        {DataBaseConnectionType.MsSql2008, typeof(MsSqlDialect)},
-                        {DataBaseConnectionType.MsSql2012, typeof(MsSqlDialect)},
-                        {DataBaseConnectionType.MySql, typeof(MySqlDialect)},
-                        {DataBaseConnectionType.SqlLite, typeof(SqliteDialect)}
-                    };
-                    wireup = persistanceWireup
-                        .WithDialect( (ISqlDialect) Activator.CreateInstance( dialectTypeMap[configuration.DataBaseConnectionType] ) )
-                        .PageEvery( 1024 )
-                        .InitializeStorageEngine()
-                        .UsingJsonSerialization();
-                }
-            }
-
-            if ( loggerFactory != null )
-            {
-                wireup = wireup.LogTo( type => loggerFactory.Create<NEventStoreAdapter>() );
-            }
-
-            StoreEvents = new AsynchronousDispatchSchedulerWireup(
-                    wireup,
-                    new DelegateMessageDispatcher( DispatchCommit ),
-                    DispatcherSchedulerStartup.Auto )
-                .UsingEventUpconversion()
-                //.WithConvertersFrom( AppDomain.CurrentDomain.GetAssemblies() /*.Where( a => a.FullName.StartsWith( "Code.CL.Domain" ) )*/.ToArray() )
-                .Build();
         }
 
-        private void DispatchCommit( ICommit commit )
+        public void AppendToStream( string streamId, IMessage message )
         {
-            foreach ( var message in commit.Events )
-            {
-                message.ProcessEvent( e =>
-                {
-                    e.Version = commit.StreamRevision;
-                    e.BucketId = commit.BucketId;
-                    e.CheckpointToken = commit.CheckpointToken;
-
-                    //MessagePublished.Execute( this, new MessageEventArgs( e ) );
-                } );
-            }
+            throw new NotImplementedException();
         }
 
-        public event EventHandler<MessageEventArgs> MessagePublished;
-
-        public void AppendToStream( IMessage message )
+        public void AppendToStream( string streamId, params IMessage[] messages )
         {
-            using ( var stream = StoreEvents.CreateStream( message.StreamId ) )
-            {
-                stream.Add( new EventMessage { Body = message } );
-                stream.CommitChanges( IdGenerator.NewGuid() );
-            }
+            throw new NotImplementedException();
         }
     }
 }
