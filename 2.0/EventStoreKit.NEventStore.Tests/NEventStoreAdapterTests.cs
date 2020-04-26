c165cb59e51125322ee7793ccdc94880df6f77dd
svjatpro <svjatpro@gmail.com>
2020-04-26 14:52:03 +0300

    migrate to new NEventStore

--- 2.0/EventStoreKit.NEventStore.Tests/NEventStoreAdapterTests.cs
+++ 2.0/EventStoreKit.NEventStore.Tests/NEventStoreAdapterTests.cs
@@ -22,18 +22,18 @@ namespace EventStoreKit.NEventStore.Tests
         public void EventStoreTest()
         {
             var store = Wireup.Init()
-                //.UsingSqlPersistence( new MySqlClientFactory(), "neventstore" )
-                .UsingSqlPersistence( new MySqlClientFactory(), "Server=127.0.0.1;Port=3306;Database=dev.tests;Uid=root;Pwd=Vfhszyf1!;charset=utf8;AutoEnlist=false;" )
-                //.UsingSqlPersistence( new NetStandardConnectionFactory( SqlClientFactory.Instance, "neventstore" ) )
-                    //"Server=127.0.0.1;Port=3306;Database=dev.tests;Uid=root;Pwd=Vfhszyf1!;charset=utf8;AutoEnlist=false;" ) )
-                //.UsingSqlPersistence( new NetStandardConnectionFactory(SqlClientFactory.Instance, "osbb" ))
-                .WithDialect( new MySqlDialect() )
                 //.UsingInMemoryPersistence()
+
+                //.UsingSqlPersistence( new MySqlClientFactory(), "Server=127.0.0.1;Port=3306;Database=dev.tests;Uid=root;Pwd=Vfhszyf1!;charset=utf8;AutoEnlist=false;" )
+                //.WithDialect( new MySqlDialect() )
+
+                .UsingSqlPersistence( new NetStandardConnectionFactory( 
+                    SqlClientFactory.Instance, 
+                    "data source=localhost;initial catalog = dev.tests; persist security info = True;user id = sa; password = Db,ktfnm1!"))
+                .WithDialect( new MsSqlDialect() )
+                
                 .InitializeStorageEngine()
                 .UsingJsonSerialization()
-                //.Compress()
-                //.EncryptWith(  )
-                //.HookIntoPipelineUsing()
                 .Build();
 
             var id = Guid.NewGuid().ToString();
@@ -53,9 +53,12 @@ namespace EventStoreKit.NEventStore.Tests
             {
                 foreach ( var @event in stream.CommittedEvents )
                 {
+
                     Console.WriteLine( ( (Message1) @event.Body ).Text );
                 }
             }
         }
+
+
     }
 }
